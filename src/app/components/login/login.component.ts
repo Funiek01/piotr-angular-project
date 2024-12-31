34c7f543855f1c06cb5ee1b255222b2f5e11755e
Funiek01 <piotrr473@gmail.com>
2024-12-31 15:02:32 +0100

    Clean-up + working with login panel

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  showPassword:boolean = false;
+  loginForm?:FormGroup;
 
+
+  constructor(private router: Router){}
+
+  togglePassword():void{
+    this.showPassword = !this.showPassword;
+  }
 }
