a9db749573ca5412adf941354f6fdf76d9037a21
Funiek01 <piotrr473@gmail.com>
2025-01-21 15:07:03 +0100

    Working on UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { HomeComponent } from './components/home/home.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
 import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { AboutComponent } from './components/about/about.component';
+
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent},
@@ -13,5 +15,6 @@ export const routes: Routes = [
     { path: 'details/:id', component:UserDetailsComponent },
     { path: 'home', component: HomeComponent},
     { path: 'edit/:id', component:EditUserComponent },
+    { path: 'about', component:AboutComponent },
     { path: '**', redirectTo: '/home' },
 ];
