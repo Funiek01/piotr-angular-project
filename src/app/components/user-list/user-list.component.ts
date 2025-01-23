087bc7c06e1bd5f052015772bfbd0c719bb7af7e
Funiek01 <piotrr473@gmail.com>
2025-01-23 09:00:20 +0100

    Added searching for users

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -6,16 +6,22 @@ import { CommonModule } from '@angular/common';
 import { ApiService } from '../../services/api.service';
 import { DTO } from '../../models/dto';
 import { UserDetailsComponent } from "../user-details/user-details.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [RouterLink, CommonModule, UserDetailsComponent],
+  imports: [RouterLink, CommonModule, UserDetailsComponent, FormsModule],
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent {
   userList: User[]=[];
+
+  filterUserList: User[] = [];
+  searchText : string = '';
+
+
   readonly colors = ['#007bff', '#6c757d', '#28a745', '#dc3545', '#ffc107', '#17a2b8'];
 
   constructor(
@@ -30,6 +36,7 @@ export class UserListComponent {
   getUsers(){
     this.apiService.request<DTO<User[]>>('userList','get').subscribe((users:DTO<User[]>)=>{
       this.userList= users['data'];
+      this.filterUserList = this.userList;
     }
     )
   }
@@ -40,6 +47,28 @@ export class UserListComponent {
 
   getColor(index: number): string {
     return this.colors[index % this.colors.length];
+  }
+
+
+  searchUser(){
+    console.log("searchText:",this.searchText);
+    const text = this.searchText.toLocaleLowerCase();
+    this.filterUserList = this.userList;
+    if(this.searchText){
+      this.filterUserList = this.userList.filter( user => {
+        return (
+          user.first_name.toLocaleLowerCase().includes(text) ||
+          user.username.toLocaleLowerCase().includes(text)
+        )
+      })
+      console.log("filter user list:",this.filterUserList);
+      return;
+    }
+  }
+
+  clearSearch(){
+    this.searchText='';
+    this.searchUser();
   }
 
 }
